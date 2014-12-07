ab43e9edb8a50e0566c2f666ef47ec58cd0d2770
Qaiser Abbasi <qaiser.abbasi@novatec-gmbh.de>
2014-12-07 12:23:32 +0100

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Redirect(routes.Products.list())
+  }
+
+  def hello(message: String) = Action {
+    Ok(views.html.hello(message))
+  }
+
+}
---
+++ build.sbt
@@ -0,0 +1,9 @@
+import play.Project._
+
+name := "Learn-Play"
+
+version := "1.0"
+
+playScalaSettings
+
+libraryDependencies += "net.sf.barcode4j" % "barcode4j" % "2.0" % "compile"
